2fd11a1e2af6b460bd3f0afbfec818ea4a3f11cc
Neelectric <Neel.R@web.de>
2024-04-03 21:49:47 +0000

    Further work getting Llama-2 to behave

--- src/biollama.py
+++ src/biollama.py
@@ -9,7 +9,9 @@ model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", de
 print("model loaded!")
 
 prompt = "In the era of generative AI, "
-tokenized_prompt = tokenizer.encode(prompt, return_tensors = "pt")
-model_output = model.generate(tokenized_prompt,
-                            max_new_tokens = 25,
-                            )
+tokenized_prompt = tokenizer.encode(prompt, return_tensors = "pt").to('cuda')
+raw_output = model.generate(tokenized_prompt,
+                            max_new_tokens = 50,
+                            temperature = 0.01)
+untokenized_output = tokenizer.decode(raw_output[0], skip_special_tokens = True)
+print(untokenized_output)
