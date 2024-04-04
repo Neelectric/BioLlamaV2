9a5cb004fc3b9b0ab5beda8c8711a01412a9496e
Neelectric <Neel.R@web.de>
2024-04-04 18:45:52 +0000

    Minor changes to llm.py

--- src/llm.py
+++ src/llm.py
@@ -3,3 +3,13 @@
 
 from biollama import BioLlama
 from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+
+def llm(self, 
+        model_path: str,
+        torch_dtype: torch.dtype,
+        RETRO_layer_ids: List[int],
+        training: bool,
+        retriever_name: str,
+        db_name: str,
+        neighbour_length: int,)
