a16bb6782b713f2cea8149cad641434fc9b2e1d7
Neelectric <Neel.R@web.de>
2024-04-12 12:23:51 +0000

    Further work getting the embeddings to work

--- src/embed_pma.py
+++ src/embed_pma.py
@@ -7,6 +7,7 @@ import os
 import numpy as np
 from tqdm import tqdm
 import json
+import glob
 
 # Helper function to return 32 token chunks from prompt
 def split_into_chunks(text, max_chunk_length=32):
@@ -18,7 +19,7 @@ def split_into_chunks(text, max_chunk_length=32):
     for i in range(0, len(tokens), max_chunk_length):
          if (i+max_chunk_length) <= len(tokens):
             chunk = tokens[i:i+max_chunk_length]
-            decoded = llama2_tokenizer.decode(chunk, skip_special_tokens=True)
+            decoded = llama2_tokenizer.decode(chunk, skip_special_tokens=True) # crucial to not add "<s>"
             chunks.append(decoded)
     return chunks
 
@@ -31,56 +32,54 @@ embeds_dir = '/root/nfs/pubmed_cleaned_embeds'
 os.makedirs(embeds_dir, exist_ok=True)
 
 
-tsv_basename = os.path.basename('/root/nfs/pubmed_cleaned/abs_1_0.tsv').split(".")[0]
+# tsv_basename = os.path.basename('/root/nfs/pubmed_cleaned/abs_1_0.tsv').split(".")[0]
 
+# def blocks(files, size=65536):
+#     while True:
+#         b = files.read(size)
+#         if not b: break
+#         yield b
 
-def blocks(files, size=65536):
-    while True:
-        b = files.read(size)
-        if not b: break
-        yield b
+# with open('/root/nfs/pubmed_cleaned/abs_1_0.tsv', "r") as f:
+#     print(sum(bl.count("\n") for bl in blocks(f)))
 
-with open('/root/nfs/pubmed_cleaned/abs_1_0.tsv', "r") as f:
-    print(sum(bl.count("\n") for bl in blocks(f)))
-
-batch_size = 128
-all_embeddings = []
-all_chunks = []
-
-count = 0
-
-with open('/root/nfs/pubmed_cleaned/abs_1_0.tsv', 'r') as file:
-    all_chunks = []
-    lines = file.readlines()
-    for start_idx in tqdm(range(0, len(lines), batch_size)):
-        if count == 25:
-            break
-        end_idx = min(start_idx + batch_size, len(lines))
-        batch_abstracts = [line.strip() for line in lines[start_idx:end_idx]]
-        
-        batch_chunks = [split_into_chunks(abstract) for abstract in batch_abstracts]
-        for abstract in batch_abstracts:
-            chunks = split_into_chunks(abstract)
-            if chunks != []:
-                all_chunks += chunks
-        count += 1
-        
-        
-    tokens = document_tokenizer(all_chunks, padding=True, return_tensors="pt")
-    input_ids = tokens.input_ids.to("cuda")
-        
-
-    with torch.no_grad():
-        embeds = document_model(input_ids).last_hidden_state[:, 0, :]
-        all_embeddings.extend(embeds.cpu().numpy())
-
-embeds_path = os.path.join(embeds_dir, f"{tsv_basename}.npy")
-np.save(embeds_path, np.array(all_embeddings))
-
+source_files = glob.glob("/root/nfs/pubmed_cleaned/*.tsv")
 lookup_table = {}
 lookup_count = 0
-for (chunk, embed) in zip(all_chunks,all_embeddings):
-    lookup_table[lookup_count] = chunk
-    lookup_count += 1
+
+for source_file in tqdm(source_files):
+    # print(f"Processing {source_file}")
+    batch_size = 128
+    all_embeddings = []
+    all_chunks = []
+    tsv_basename = os.path.basename(source_file).split(".")[0]
+
+    with open(source_file, 'r') as file:
+        all_chunks = []
+        lines = file.readlines()
+        for start_idx in tqdm(range(0, len(lines), batch_size), disable=True):
+            end_idx = min(start_idx + batch_size, len(lines))
+            batch_abstracts = [line.strip() for line in lines[start_idx:end_idx]]
+            batch_chunks = [split_into_chunks(abstract) for abstract in batch_abstracts]
+            for abstract in batch_abstracts:
+                chunks = split_into_chunks(abstract)
+                if chunks != []:
+                    all_chunks += chunks
+            
+        tokens = document_tokenizer(all_chunks, padding=True, return_tensors="pt")
+        input_ids = tokens.input_ids.to("cuda")
+        step_size = 50
+        for i in range(0, len(input_ids), step_size):
+            temp_input_ids = input_ids[i:i+step_size]
+            with torch.no_grad():
+                embeds = document_model(temp_input_ids).last_hidden_state[:, 0, :]
+                all_embeddings.extend(embeds.cpu().numpy())
+
+    embeds_path = os.path.join(embeds_dir, f"{tsv_basename}.npy")
+    np.save(embeds_path, np.array(all_embeddings))
+
+    for (chunk, embed) in zip(all_chunks,all_embeddings):
+        lookup_table[lookup_count] = chunk
+        lookup_count += 1
 with open('/root/nfs/pubmed_cleaned_index/lookup_table.json', 'w') as json_file:
     json.dump(lookup_table, json_file)
--- src/test_faiss_index.py
+++ src/test_faiss_index.py
@@ -4,11 +4,21 @@ import numpy as np
 import torch
 from transformers import AutoTokenizer, AutoModel
 import json
+from time import time
 
 embeddings_dir = '/root/nfs/pubmed_cleaned_embeds/'
 index_path = '/root/nfs/pubmed_cleaned_index/'
 
+# load all files under /root/nfs/pubmed_cleaned/ as a list
+embeds_files = glob.glob("/root/nfs/pubmed_cleaned/*.tsv")
+print(embeds_files)
+
+time_before_index_load = time()
 my_index = faiss.read_index(index_path + "medcpt_index.faiss")
+time_after_index_load = time()
+time_to_load_index = time_after_index_load - time_before_index_load
+print(f"Time to load index: {time_to_load_index}")
+
 
 model = AutoModel.from_pretrained("ncbi/MedCPT-Query-Encoder")
 tokenizer = AutoTokenizer.from_pretrained("ncbi/MedCPT-Query-Encoder")
@@ -29,22 +39,36 @@ with torch.no_grad():
 	)
 	
 	embeds = model(**encoded).last_hidden_state[:, 0, :]
-	print(embeds)
 	print(embeds.size())
+
+time_after_encoding = time()
+time_to_encode = time_after_encoding - time_after_index_load
+print(f"Time to encode: {time_to_encode}")
 
 k = 5
 distances, indices = my_index.search(embeds, k)
+time_after_search = time()
+time_to_search = time_after_search - time_after_encoding
+print(f"Time to search: {time_to_search}")
+
 # temp = my_index.search_level_0(embeds, k) # missing 5 required positional arguments: 'k', 'nearest', 'nearest_d', 'distances', and 'labels'
 # temp3 = my_index.search_c(embeds,k) # missing 3 required positional arguments: 'k', 'distances', and 'labels'
 # temp4 = my_index.search_and_reconstruct(embeds, k)
 # temp5 = my_index.range_search(embeds, k)
 
-print(list(zip(distances[0], indices[0])))
+# print(list(zip(distances[0], indices[0])))
 
 with open('/root/nfs/pubmed_cleaned_index/lookup_table.json', "r") as file:
 	lookup_table = json.load(file)
 	
-for index in indices:
+neighbors = []
+for index in indices[0]:
 	print(index)
-	index = int(index)
+	index = str(index)
 	print(lookup_table[index])
+	neighbors.append(lookup_table[index])
+	
+llama2_tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
+tokenized = llama2_tokenizer(neighbors)
+for neighbor in tokenized:
+	print(neighbor)
