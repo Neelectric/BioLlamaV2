10e2e0592f1bd669bd40d94a9533af4561d03ea0
Neelectric <Neel.R@web.de>
2024-04-14 12:31:59 +0000

    Still failing to embed second half...

--- test_and_build_scripts/embed_pma.py
+++ test_and_build_scripts/embed_pma.py
@@ -26,12 +26,12 @@ def split_into_chunks(text, total_token_count, max_chunk_length=32):
     return chunks, total_token_count
 
 llama_path = "meta-llama/Llama-2-7b-chat-hf"
-# medcpt_path = "ncbi/MedCPT-Article-Encoder"
+medcpt_path = "ncbi/MedCPT-Article-Encoder"
 llama2_tokenizer = AutoTokenizer.from_pretrained(llama_path, cache_dir = "../hf_cache/")
-# document_tokenizer = AutoTokenizer.from_pretrained(medcpt_path)
-# document_model = AutoModel.from_pretrained(medcpt_path).to('cuda')
-# embeds_dir = '/root/nfs/pubmed_cleaned_embeds'
-# os.makedirs(embeds_dir, exist_ok=True)
+document_tokenizer = AutoTokenizer.from_pretrained(medcpt_path)
+document_model = AutoModel.from_pretrained(medcpt_path).to('cuda:1')
+embeds_dir = '/root/nfs/pubmed_cleaned_embeds'
+os.makedirs(embeds_dir, exist_ok=True)
 
 
 source_files = glob.glob("/root/nfs/pubmed_cleaned/*.tsv")
@@ -39,41 +39,42 @@ lookup_table = {}
 lookup_count = 0
 # file_count = 0
 
-for source_file in tqdm(source_files):
-    batch_size = 128
-    all_embeddings = []
-    all_chunks = []
-    tsv_basename = os.path.basename(source_file).split(".")[0]
-    # if file_count == 2:
-    #     break
-
-    with open(source_file, 'r') as file:
+for i, source_file in tqdm(enumerate(source_files)):
+    if i >= 15:
+        batch_size = 128
+        all_embeddings = []
         all_chunks = []
-        lines = file.readlines()
-        for start_idx in tqdm(range(0, len(lines), batch_size)):
-            end_idx = min(start_idx + batch_size, len(lines))
-            batch_abstracts = [line.strip() for line in lines[start_idx:end_idx]]
-            for abstract in batch_abstracts:
-                chunks, total_token_count = split_into_chunks(abstract, total_token_count)
-                if chunks != []:
-                    all_chunks += chunks
-            
-    #     tokens = document_tokenizer(all_chunks, padding=True, return_tensors="pt")
-    #     input_ids = tokens.input_ids.to("cuda")
-    #     step_size = 50
-    #     for i in tqdm(range(0, len(input_ids), step_size)):
-    #         temp_input_ids = input_ids[i:i+step_size]
-    #         with torch.no_grad():
-    #             embeds = document_model(temp_input_ids).last_hidden_state[:, 0, :]
-    #             all_embeddings.extend(embeds.cpu().numpy())
+        tsv_basename = os.path.basename(source_file).split(".")[0]
+        # if file_count == 2:
+        #     break
 
-    # embeds_path = os.path.join(embeds_dir, f"{tsv_basename}.npy")
-    # np.save(embeds_path, np.array(all_embeddings))
+        with open(source_file, 'r') as file:
+            all_chunks = []
+            lines = file.readlines()
+            for start_idx in tqdm(range(0, len(lines), batch_size), disable=False):
+                end_idx = min(start_idx + batch_size, len(lines))
+                batch_abstracts = [line.strip() for line in lines[start_idx:end_idx]]
+                for abstract in batch_abstracts:
+                    chunks, total_token_count = split_into_chunks(abstract, total_token_count)
+                    if chunks != []:
+                        all_chunks += chunks
+                
+            tokens = document_tokenizer(all_chunks, padding=True, return_tensors="pt")
+            input_ids = tokens.input_ids.to("cuda:1")
+            step_size = 50
+            for i in tqdm(range(0, len(input_ids), step_size)):
+                temp_input_ids = input_ids[i:i+step_size]
+                with torch.no_grad():
+                    embeds = document_model(temp_input_ids).last_hidden_state[:, 0, :]
+                    all_embeddings.extend(embeds.cpu().numpy())
 
-    for chunk in all_chunks:
-        lookup_table[lookup_count] = chunk
-        lookup_count += 1
+        embeds_path = os.path.join(embeds_dir, f"{tsv_basename}.npy")
+        np.save(embeds_path, np.array(all_embeddings))
+
+        for chunk in all_chunks:
+            lookup_table[lookup_count] = chunk
+            lookup_count += 1
     # file_count += 1
-with open('/root/nfs/pubmed_cleaned_index/lookup_table_definitive.json', 'w') as json_file:
+with open('/root/nfs/pubmed_cleaned_index/lookup_table_1.json', 'w') as json_file:
     json.dump(lookup_table, json_file)
 print(total_token_count)
