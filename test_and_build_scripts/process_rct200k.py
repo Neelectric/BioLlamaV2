b72925f9e6bfe250e023f02368568aa92f1868c2
Neelectric <Neel.R@web.de>
2024-06-06 21:48:57 +0000

    Can't currently dump json as tensors are not JSON serializable. Will just create medCPT embedding directly and add to .npy files instead

--- test_and_build_scripts/process_rct200k.py
+++ test_and_build_scripts/process_rct200k.py
@@ -1,6 +1,9 @@
 ### open the txt file in rct200k/train.txt
 
 from tqdm import tqdm
+import json
+from transformers import AutoTokenizer
+import torch
 
 # Path: test_and_build_scripts/process_rct200k.py
 with open('rct200k/train.txt', 'r') as f:
@@ -33,8 +36,41 @@ for abstract_id, abstract in abstracts.items():
     #if the string contains "Unique identifier NCT", then chop this off and everything after it
     if "Unique identifier NCT" in abstract:
         abstract = abstract.split("Unique identifier NCT")[0]
+        # print("   NCT found")
     if "ACTRN" in abstract:
         abstract = abstract.split("ACTRN")[0]
+        # print("   ACTRN found")
     abstracts[abstract_id] = abstract
-    print(abstracts[abstract_id])
-    print("-"*50)
+
+print("all abstracts cleaned.")
+
+# with open('rct200k/train_cleaned.json', 'w') as f:
+#     json.dump(abstracts, f)
+
+llama_path = "meta-llama/Llama-2-7b-chat-hf"
+tokenizer = AutoTokenizer.from_pretrained(llama_path, cache_dir = "../hf_cache/")
+tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+
+# now we need to tokenize each abstract, split each abstract into chunks of 32 tokens, and save each chunk to an id in a JSON
+# we'll also save the abstract_id that the chunk came from
+# if the last chunk is less than 32 tokens, we'll skip it
+abstract_chunks = {}
+for abstract_id, abstract in tqdm(abstracts.items()):
+    tokens = tokenizer(abstract, return_tensors="pt", padding=True, truncation=True, max_length=512)
+    input_ids = tokens["input_ids"]
+    for i in range(1, input_ids.size(1), 32):
+        chunk = input_ids[0, i:i+32]
+        # also, if current chunk contains pad tokens, skip it
+        if tokenizer.pad_token_id in chunk:
+            print(f"skipping chunk {i} in abstract {abstract_id} because it contains pad tokens")
+            continue
+        if torch.any(chunk == tokenizer.pad_token_id):
+            print(f"skipping chunk {i} in abstract {abstract_id} because it contains pad tokens")
+            continue
+        if chunk.size(0) == 32:
+            detokenized_chunk = tokenizer.decode(chunk)
+            abstract_chunks[len(abstract_chunks)] = {"abstract_id": abstract_id, "chunk": chunk, "detokenized_chunk": detokenized_chunk}
+            
+
+with open('rct200k/train_chunks.json', 'w') as f:
+    json.dump(abstract_chunks, f)
